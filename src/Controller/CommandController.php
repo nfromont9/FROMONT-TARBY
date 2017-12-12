c9eed88869373651d626c51576c21caad8be6c8c
marvin <marvin.tarby@gmail.com>
2017-12-12 12:10:34 +0100

    BackOffice - Affichage des commandes

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -31,11 +31,26 @@ class CommandController implements ControllerProviderInterface{
         return $app["twig"]->render("backOff/Commande/showCommandes.html.twig", ['data'=>$commandes]);
     }
 
+    public function validCommande(Application $app, $id) {
+        $this->commandeModel = new CommandeModel($app);
+        $commandes = $this->commandeModel->validCommande($id);
+        return $app["twig"]->render("backOff/Commande/showCommandes.html.twig", ['data'=>$commandes]);
+    }
+
+    public function deleteCommande(Application $app, $id) {
+        $this->commandeModel = new CommandeModel($app);
+        $commandes = $this->commandeModel->deleteCommande($id);
+        return $app["twig"]->render("backOff/Commande/showCommandes.html.twig", ['data'=>$commandes]);
+    }
+
     public function connect(Application $app) {
         $controllers = $app['controllers_factory'];
 
         $controllers->get('/', 'App\Controller\commandController::index')->bind('commande.index');
+
         $controllers->get('/showCo', 'App\Controller\commandController::showCommandes')->bind('commande.showCommandes');
+        $controllers->post('/validCo', 'App\Controller\commandController::validCommande')->bind('commande.validCommande');
+        $controllers->get('/deleteCo', 'App\Controller\commandController::deleteCommande')->bind('commande.deleteCommande');
 
 
         return $controllers;
